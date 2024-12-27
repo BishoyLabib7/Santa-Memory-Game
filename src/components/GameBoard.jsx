82800a4ccbd10af416529c687e94cbfa6436dcf5
Bishoy Labib <132815670+BishoyLabib7@users.noreply.github.com>
2024-12-27 23:41:51 +0200

    GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,12 @@
 import IconCard from "./IconCard";
 import { GameContext } from "../context/GameContextProvider";
-import "./responsive.css";
 import { useContext } from "react";
 
 const GameBoard = () => {
   const { cards } = useContext(GameContext);
 
   return (
-    <div className="gameboard absolute grid grid-cols-4 gap-3">
+    <div className="flex items-center justify-center grid grid-cols-4 gap-3">
       {cards.map((icon, index) => (
         <IconCard key={index} iconId={index} />
       ))}
