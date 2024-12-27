f39936995ebec92c1ac00ba7430ff8fd8d8801ee
bishoy labib <bishoylabib78@gmail.com>
2024-12-27 22:24:25 +0200

    ButtonMenu.jsx

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -8,7 +8,7 @@ const ButtonMenu = () => {
   const { timeLeft, restartGame } = useContext(GameContext);
 
   return (
-    <div className=" buttonmenu absolute md:right-[10rem] md:top-[20rem] flex flex-col items-start w-[18rem]">
+    <div className=" buttonmenu absolute lg:right-[10rem] lg:top-[20rem] flex lg:flex-col flex-row items-start w-[18rem]">
       <div className="flex justify-center  items-center gap-3 bg-button-background bg-center bg-contain bg-no-repeat w-full h-[5rem]">
         <img src={ClockBtn} alt="" className=" h-10" />
         <p className=" text-white md:text-lg font-bold">{timeLeft} secs</p>
