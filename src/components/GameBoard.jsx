415bd32e1e4ed2dbcff74468d234d605267cb903
bishoy labib <bishoylabib78@gmail.com>
2024-12-27 22:13:07 +0200

    gameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,19 +1,18 @@
-import { useContext } from "react";
-import { GameContext } from "../context/GameContextProvider";
 import IconCard from "./IconCard";
+import { GameContext } from "../context/GameContextProvider";
 import "./responsive.css";
+import { useContext } from "react";
 
-function GameBoard() {
+const GameBoard = () => {
   const { cards } = useContext(GameContext);
+
   return (
-    <div className="absolute flex items-center justify-center">
-      <div className="grid grid-cols-4 gap-3">
-        {cards.map((card, i) => (
-          <IconCard cardId={card.id} icon={card.icon} key={i} />
-        ))}
-      </div>
+    <div className="gameboard absolute grid grid-cols-4 gap-3">
+      {cards.map((icon, index) => (
+        <IconCard key={index} iconId={index} />
+      ))}
     </div>
   );
-}
+};
 
 export default GameBoard;
