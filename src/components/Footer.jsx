6d1b7714c3d719608cdba0b795d29ea7ad6c14b7
bishoy labib <bishoylabib78@gmail.com>
2024-12-27 22:12:53 +0200

    footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="absolute bottom-0  text-red-950 my-4 font-serif  text-1xl xl:text-2xl flex items-center justify-center">
+    <div className="absolute bottom-0 text-red-950 font-serif text-1xl xl:text-2xl flex items-center justify-center">
       &copy; Copyright by Bishoy Labib 2025
     </div>
   );
