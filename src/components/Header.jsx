4f8b7ba7ce3237ffbd6f2d37944b259c2d367fb4
bishoy labib <bishoylabib78@gmail.com>
2024-12-29 15:10:40 +0200

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,11 @@ import { GameContext } from "../context/GameContextProvider";
 function Header() {
   const { name } = useContext(GameContext);
   return (
-    <div className=" absolute top-0 text-red-200 my-4 font-Christmas  text-center text-4xl xl:text-6xl  flex items-center justify-center">
-      Merry Christams {name ? name : "2025"}
+    <div className="absolute top-0 text-red-200 my-4 font-Christmas flex tems-center justify-center">
+      <h1 className="font-bold text-center text-3xl xl:text-7xl   ">
+        {" "}
+        Merry Christams {name ? name : "2025"}
+      </h1>
     </div>
   );
 }
