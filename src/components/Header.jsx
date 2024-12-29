1ce25cf714bdd8f90e0b573f530d623ef35f492d
Bishoy Labib <132815670+BishoyLabib7@users.noreply.github.com>
2024-12-29 15:17:45 +0200

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,17 @@ import { GameContext } from "../context/GameContextProvider";
 
 function Header() {
   const { name } = useContext(GameContext);
+  const data = new Date();
   return (
     <div className="absolute top-0 text-red-200 my-4 font-Christmas flex tems-center justify-center">
       <h1 className="font-bold text-center text-3xl xl:text-7xl   ">
         {" "}
-        Merry Christams {name ? name : "2025"}
+        Merry Christams{" "}
+        {name
+          ? name
+          : data.getMonth() === 11
+          ? data.getFullYear() + 1
+          : data.getFullYear()}
       </h1>
     </div>
   );
