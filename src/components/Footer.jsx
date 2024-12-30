8a80f91a0ee5a6e8bd280175cfee982288d800c6
bishoy labib <bishoylabib78@gmail.com>
2024-12-30 16:25:39 +0200

    Footer

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="absolute bottom-0 text-red-950 font-serif text-1xl xl:text-2xl flex items-center justify-center">
-      &copy; Copyright by Bishoy Labib 2025
-    </div>
-  );
-}
-
-export default Footer;
