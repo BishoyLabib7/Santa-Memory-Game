04f005d133b373b43ce530403b831e6d380ba35f
Bishoy Labib <132815670+BishoyLabib7@users.noreply.github.com>
2024-12-30 22:18:40 +0200

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,7 @@ function Header() {
   return (
     <div className="absolute top-0 text-red-200 my-4 font-Christmas flex tems-center justify-center">
       <h1 className="font-bold text-center text-3xl xl:text-7xl   ">
-        {" "}
-<<<<<<< HEAD
-        Happy New Year{" "}
-=======
-        Merry Christmas{" "}
->>>>>>> 15ae3d6ac0247fd5ab53e98f4d5ea4f2b0886df8
-        {name
+        Happy New Year {name
           ? name
           : data.getMonth() === 11
           ? data.getFullYear() + 1
