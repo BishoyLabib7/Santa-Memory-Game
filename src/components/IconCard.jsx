d4b9481570716ffd80fcc3416a8defe0309e0c5f
bishoy labib <bishoylabib78@gmail.com>
2024-12-30 22:15:03 +0200

    IconCard.jsx

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { GameContext } from "../context/GameContextProvider";
+import santa from "../assets/images/icons/santa-icon.png";
 import "./IconCard.css";
 
 const IconCard = ({ iconId }) => {
@@ -14,15 +15,15 @@ const IconCard = ({ iconId }) => {
   return (
     <div
       onClick={handleClick}
-      className="relative w-[4rem] h-[4rem] md:w-[4.5rem] md:h-[4.5rem] flex justify-center items-center cursor-pointer perspective-1000"
+      className="relative w-[3.5rem] h-[3.5rem] md:w-[4rem] md:h-[4rem] flex justify-center items-center cursor-pointer perspective-1000"
     >
       <div
         className={`flip-card-inner ${
           card.flipped || showIcons ? "flip-card-flipped" : ""
         }`}
       >
-        <div className="flip-card-front bg-white flex justify-center items-center rounded-full">
-          <p className="text-3xl font-bold text-red-600">?</p>
+        <div className="flip-card-front bg-white border flex justify-center items-center rounded-full">
+          <img src={santa} alt="" className="" />
         </div>
         <div className="flip-card-back bg-ball-background bg-center bg-cover flex justify-center items-center">
           <img src={card.icon} alt={card.icon} className=" w-14 object-cover" />
