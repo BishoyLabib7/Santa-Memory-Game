7d01ce4419bdf1b184794ddfa8248c1d3647457e
bishoy labib <bishoylabib78@gmail.com>
2024-12-30 16:25:06 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import GameBoard from "./components/GameBoard";
 import ButtonMenu from "./components/ButtonMenu";
 import Result from "./components/Result";
-import GameContextProvider from "./context/GameContextProvider";
 import Sound from "./components/Sound";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Login from "./components/Login";
 import { useContext } from "react";
 import { GameContext } from "./context/GameContextProvider";
@@ -12,21 +10,20 @@ import { GameContext } from "./context/GameContextProvider";
 function App() {
   const { start } = useContext(GameContext);
   return (
-    <main className="relative w-screen h-screen flex items-center justify-center bg-center bg-cover bg-app-banner overflow-hidden">
+    <main className="relative w-screen h-screen flex justify-center items-center bg-app-banner bg-center bg-cover overflow-hidden">
       <Header />
       <Sound />
       {!start ? (
         <Login />
       ) : (
         <>
-          <div className="flex justify-center items-center bg-gameboard-background w-[36rem] h-[36rem] md:w-[40rem] md:h-[40rem] bg-center bg-contain bg-no-repeat">
+          <div className="relative flex justify-center items-center w-[36rem] h-[36rem] md:w-[40rem] md:h-[40rem] bg-gameboard-background bg-center bg-contain bg-no-repeat">
             <GameBoard />
           </div>
           <ButtonMenu />
           <Result />
         </>
       )}
-      <Footer />
     </main>
   );
 }
