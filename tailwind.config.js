a27624ef8cd1719b7801d9f3ceb1c837dfdaaba9
bishoy labib <bishoylabib78@gmail.com>
2024-12-30 22:15:41 +0200

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         "gameboard-background": "url(assets/images/gameboard.png)",
         "ball-background": "url(assets/images/ball-bg.png)",
         "button-background": "url(assets/images/button-bg.png)",
+        santa_icon: "url(assets/images/icon/santa-icon.png)",
       },
       fontFamily: {
         Christmas: ["MountainsofChristmas-Bold", "Cursive"],
