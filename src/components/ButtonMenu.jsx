bffa77c394d36b66af101a4a1ce9279a63197c3b
bishoy labib <bishoylabib78@gmail.com>
2024-12-30 16:25:22 +0200

    ButtonMenu.jsx

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -8,15 +8,15 @@ const ButtonMenu = () => {
   const { timeLeft, restartGame } = useContext(GameContext);
 
   return (
-    <div className=" buttonmenu absolute lg:right-[10rem] lg:top-[20rem] flex lg:flex-col flex-row items-start w-[18rem]">
-      <div className="flex justify-center  items-center gap-3 bg-button-background bg-center bg-contain bg-no-repeat w-full h-[5rem]">
-        <img src={ClockBtn} alt="" className=" h-10" />
+    <div className=" buttonmenu absolute md:right-[10rem] md:top-[20rem] flex flex-row lg:flex-col items-start w-[18rem]">
+      <div className="flex justify-center items-center gap-3 bg-button-background bg-center bg-contain bg-no-repeat w-full h-[5rem]">
+        <img src={ClockBtn} alt="" className="h-6 lg:h-10" />
         <p className=" text-white md:text-lg font-bold">{timeLeft} secs</p>
       </div>
 
       <div className="flex justify-center items-center gap-3 bg-button-background bg-center bg-contain bg-no-repeat w-full h-[5rem]">
         <button onClick={restartGame} className="flex items-center gap-3">
-          <img src={RestartBtn} alt="" className=" w-8 h-8" />
+          <img src={RestartBtn} alt="" className=" w-5 h-5 lg:w-8 lg:h-8" />
           <p className=" text-white md:text-lg font-bold">Replay</p>
         </button>
       </div>
