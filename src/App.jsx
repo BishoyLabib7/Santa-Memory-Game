fe4c1eccfffd0bc34443467e2006ca14ec8b9d2b
bishoy labib <bishoylabib78@gmail.com>
2025-01-01 17:31:04 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import GameBoard from "./components/GameBoard";
-import ButtonMenu from "./components/ButtonMenu";
-import Result from "./components/Result";
-import Sound from "./components/Sound";
-import Header from "./components/Header";
-import Login from "./components/Login";
+import { Suspense, lazy } from "react";
 import { useContext } from "react";
+
 import { GameContext } from "./context/GameContextProvider";
+import Loader from "./components/Loader";
+
+const GameBoard = lazy(() => import("./components/GameBoard"));
+const ButtonMenu = lazy(() => import("./components/ButtonMenu"));
+const Result = lazy(() => import("./components/Result"));
+const Sound = lazy(() => import("./components/Sound"));
+const Header = lazy(() => import("./components/Header"));
+const Login = lazy(() => import("./components/Login"));
 
 function App() {
   const { start } = useContext(GameContext);
   return (
-    <main className="relative w-screen h-screen flex justify-center items-center bg-app-banner bg-center bg-cover overflow-hidden">
-      <Header />
-      <Sound />
-      {!start ? (
-        <Login />
-      ) : (
-        <>
-          <div className="relative flex justify-center items-center w-[36rem] h-[36rem] md:w-[40rem] md:h-[40rem] bg-gameboard-background bg-center bg-contain bg-no-repeat">
-            <GameBoard />
-            <ButtonMenu />
-          </div>
-          <Result />
-        </>
-      )}{" "}
-    </main>
+    <Suspense fallback={<Loader />}>
+      <main className="relative w-screen h-screen flex justify-center items-center bg-app-banner bg-center bg-cover overflow-hidden">
+        <Header />
+        <Sound />
+        {!start ? (
+          <Login />
+        ) : (
+          <>
+            <div className="relative flex justify-center items-center w-[36rem] h-[36rem] md:w-[40rem] md:h-[40rem] bg-gameboard-background bg-center bg-contain bg-no-repeat">
+              <GameBoard />
+              <ButtonMenu />
+            </div>
+            <Result />
+          </>
+        )}{" "}
+      </main>
+    </Suspense>
   );
 }
 
