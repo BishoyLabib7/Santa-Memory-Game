4791f8d02592254ccf9798ee58f690f5da138540
Bishoy <bishoylabib78@gmail.com>
2025-09-04 21:10:26 +0300

    last Update

--- src/App.jsx
+++ src/App.jsx
@@ -15,21 +15,26 @@ function App() {
   const { start } = useContext(GameContext);
   return (
     <Suspense fallback={<Loader />}>
-      <main className="relative w-screen h-screen flex justify-center items-center bg-app-banner bg-center bg-cover overflow-hidden">
-        <Header />
-        <Sound />
-        {!start ? (
-          <Login />
-        ) : (
-          <>
-            <div className="relative flex justify-center items-center w-[36rem] h-[36rem] md:w-[40rem] md:h-[40rem] bg-gameboard-background bg-center bg-contain bg-no-repeat">
-              <GameBoard />
-              <ButtonMenu />
-            </div>
-            <Result />
-          </>
-        )}{" "}
-      </main>
+      <div className=" w-screen h-screen bg-app-banner bg-center bg-cover overflow-hidden">
+        <main className="relative flex w-screen h-[95vh] justify-center items-center">
+          <Header />
+          <Sound />
+          {!start ? (
+            <Login />
+          ) : (
+            <>
+              <div className="relative flex justify-center items-center w-[36rem] h-[36rem] md:w-[40rem] md:h-[40rem] bg-gameboard-background bg-center bg-contain bg-no-repeat">
+                <GameBoard />
+                <ButtonMenu />
+              </div>
+              <Result />
+            </>
+          )}{" "}
+        </main>
+        <footer className="text-center lg:text-xl md:text-sm font-semibold text-red-700">
+          © 2025 Made❤by Bishoy
+        </footer>
+      </div>
     </Suspense>
   );
 }
